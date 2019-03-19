d40bfcb39dbd2f30e31404374b0daabf8d42af4a
@ZedAntima <antima.gupta@zedtechnosolutions.com>
2019-03-19 13:34:57 +0530

    app folder replaced and logo added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FootarComponent } from './footar/footar.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FootarComponent,
+    LandingPageComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
