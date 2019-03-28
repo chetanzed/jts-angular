4ba10310f7f1051dc45dc916ba3ab3e839e26be0
ravinderzed <ravinder.zedtechnosolutions@gmail.com>
2019-03-28 11:23:51 +0530

    new html css js change

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,174 +1,61 @@
 
+$(document).ready(function() {
+          $("#owl-demo1").owlCarousel({
+          navigation : true,
+          slideSpeed : 300,
+          paginationSpeed : 400,
+          singleItem : true,
+          //Autoplay
+            autoPlay : true,
+            stopOnHover : true,
+             navigationText: [
+            "<span class='icon-left-arrow'></span>",    
+            "<span class='icon-right-arrow'></span>"],
 
-var options = {
-    valueNames: [
-        'name',
-        'born',
-        'address',
-        { data: ['gender']}
-    ],
-    page: 12,
-    pagination: true
-};
-var userList = new List('users', options);
-var userList = new List('users1', options);
-var userList = new List('users2', options);
+          // "singleItem:true" is a shortcut for:
+          // items : 1, 
+          // itemsDesktop : false,
+          // itemsDesktopSmall : false,
+          // itemsTablet: false,
+          // itemsMobile : false
 
-function resetList(){
-    userList.search();
-    userList.filter();
-    userList.update();
-    $(".filter-all").prop('checked', true);
-    $('.filter').prop('checked', false);
-    $('.search').val('');
-    //console.log('Reset Successfully!');
-};
-  
-function updateList(){
-  var values_gender = $("input[name=gender]:checked").val();
-    var values_address = $("input[name=address]:checked").val();
-    console.log(values_gender, values_address);
-
-    userList.filter(function (item) {
-        var genderFilter = false;
-        var addressFilter = false;
-        
-        if(values_gender == "all")
-        { 
-            genderFilter = true;
-        } else {
-            genderFilter = item.values().gender == values_gender;
-            
-        }
-        if(values_address == null)
-        { 
-            addressFilter = true;
-        } else {
-            addressFilter = item.values().address.indexOf(values_address) >= 0;
-        }
-        return addressFilter && genderFilter
-    });
-    userList.update();
-    //console.log('Filtered: ' + values_gender);
-}
-                               
-$(function(){
-  //updateList();
-  $("input[name=gender]").change(updateList);
-    $('input[name=address]').change(updateList);
-    
-    userList.on('updated', function (list) {
-        if (list.matchingItems.length > 0) {
-            $('.no-result').hide()
-        } else {
-            $('.no-result').show()
-        }
-    });
-});
-
-
-
-
-
-
-
-
-// Start upload preview image
-	$(".gambar").attr("src", "images/dummy-image.jpg");
-        var $uploadCrop,
-        tempFilename,
-        rawImg,
-        imageId;
-        function readFile(input) {
-            if (input.files && input.files[0]) {
-              var reader = new FileReader();
-                reader.onload = function (e) {
-                    $('.upload-demo').addClass('ready');
-                    $('#cropImagePop').modal('show');
-                    rawImg = e.target.result;
-                }
-                reader.readAsDataURL(input.files[0]);
-            }
-            else {
-                swal("Sorry - you're browser doesn't support the FileReader API");
-            }
-        }
-
-        $uploadCrop = $('#upload-demo').croppie({
-            viewport: {
-                width: 200,
-                height: 240,
-            },
-            enforceBoundary: false,
-            enableExif: true
-        });
-        $('#cropImagePop').on('shown.bs.modal', function(){
-            // alert('Shown pop');
-            $uploadCrop.croppie('bind', {
-                url: rawImg
-            }).then(function(){
-                console.log('jQuery bind complete');
-            });
+          });
         });
 
-        $('.item-img').on('change', function () { imageId = $(this).data('id'); tempFilename = $(this).val();
-            $('#cancelCropBtn').data('id', imageId); readFile(this); });
-        $('#cropImageBtn').on('click', function (ev) {
-            $uploadCrop.croppie('result', {
-                type: 'base64',
-                format: 'jpeg',
-                size: {width: 200, height: 240}
-            }).then(function (resp) {
-                $('#item-img-output').attr('src', resp);
-                $('#cropImagePop').modal('hide');
-            });
-    });
-// End upload preview image
+
+        $(document).ready(function() {
+          $("#owl-demo").owlCarousel({
+            navigation : true,
+            slideSpeed : 300,
+            paginationSpeed : 400,
+            singleItem : true,
+            pagination: true,
+          //Autoplay
+            autoPlay : true,
+            stopOnHover : true,
+             navigationText: [
+            "<i class='fas fa-angle-left'></i>",    
+            "<i class='fas fa-angle-right'></i>"],
+          });
+
+        });
 
 
 
+        var btn = $('#back-top-button');
 
+        $(window).scroll(function() {
+          if ($(window).scrollTop() > 300) {
+            btn.addClass('show');
+          } else {
+            btn.removeClass('show');
+          }
+        });
 
-/*$('#example1').calendar();*/
-$('#example1').calendar({
-  type: 'date'
-});
-$('#example2').calendar({
-  type: 'date'
-});
-$('#example3').calendar({
-  type: 'time'
-});
-$('#example4').calendar({
-  type: 'time'
-});
-
-/*var today = new Date();
-$('#example11').calendar({
-  minDate: new Date(today.getFullYear(), today.getMonth(), today.getDate() - 5),
-  maxDate: new Date(today.getFullYear(), today.getMonth(), today.getDate() + 5)
-});
-$('#example12').calendar({
-  monthFirst: false
-});
-$('#example13').calendar({
-  monthFirst: false,
-  formatter: {
-    date: function (date, settings) {
-      if (!date) return '';
-      var day = date.getDate();
-      var month = date.getMonth() + 1;
-      var year = date.getFullYear();
-      return day + '/' + month + '/' + year;
-    }
-  }
-});
-$('#example14').calendar({
-  inline: true
-});
-$('#example15').calendar();*/
-
-
+        btn.on('click', function(e) {
+          e.preventDefault();
+          $('html, body').animate({scrollTop:0}, '300');
+        });
 
 
 
