95e5f66f1596df252abeb5e10952d93c3285e0d6
ravinderzed <ravinder.zedtechnosolutions@gmail.com>
2019-04-09 11:07:41 +0530

    set header on top

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,6 +10,19 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    window.onscroll = function() {myFunction()};
+
+    var navbar = document.getElementById("navbar");
+    var sticky = navbar.offsetTop;
+
+    function myFunction() {
+      if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+      } else {
+        navbar.classList.remove("sticky");
+      }
+    }
   }
 
 }
