ca7a926acce008df69eed4a4200d9dc7f9e9fc42
ravinderzed <ravinder.zedtechnosolutions@gmail.com>
2019-04-10 17:05:35 +0530

    header fixed

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,7 +10,18 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    window.onscroll = function() {myFunction()};
 
+    var navbar = document.getElementById("navbar");
+    var sticky = navbar.offsetTop;
+  
+    function myFunction() {
+      if (window.pageYOffset > sticky) {
+        navbar.classList.add("sticky")
+      } else {
+        navbar.classList.remove("sticky");
+      }
+    }
     
   }
 
