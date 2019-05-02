85392ff52277ff58f3210435887e9931dd3e8145
ravinderzed <ravinder.zedtechnosolutions@gmail.com>
2019-05-02 17:34:26 +0530

    eventform validated

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -1,18 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { utilClass } from '../model/utilClass'; 
+import { utilClass } from '../model/utilClass';
 @Component({
   selector: 'app-event',
   templateUrl: './event.component.html',
   styleUrls: ['./event.component.css']
 })
 export class EventComponent extends utilClass implements OnInit {
-firstLoad: any;
-eventForm: FormGroup;
-    submitted = false;
+  firstLoad: any;
+  eventForm: FormGroup;
+  submitted = false;
   constructor(private formBuilder: FormBuilder) {
     super();
-   }
+  }
 
   ngOnInit() {
     if (this.firstLoad) {
@@ -29,32 +29,31 @@ eventForm: FormGroup;
       tel: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       salon_like_address: ['', [Validators.required]]
-  });
-
-
-
-  
-}
-// convenience getter for easy access to form fields
-get f() { return this.eventForm.controls; }
+    });
 
 
 
 
-
-
-onSubmit() {
-  this.submitted = true;
-  // stop here if form is invalid
-  if (this.eventForm.invalid) {
-      return;
   }
-console.log(this.eventForm);
-  //alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value))
-  alert('SUCCESS');
-}
-mask: any[] =
-// ['+', '1', ' ', '(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/];
-[/\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/];
+  // convenience getter for easy access to form fields
+  get f() { return this.eventForm.controls; }
+
+
+
+
+
+
+  onSubmit() {
+    this.submitted = true;
+    // stop here if form is invalid
+    if (this.eventForm.invalid) {
+      return;
+    }
+    console.log(this.eventForm);
+    //alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value))
+  }
+  mask: any[] =
+    // ['+', '1', ' ', '(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/];
+    [/\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/];
 
 }
