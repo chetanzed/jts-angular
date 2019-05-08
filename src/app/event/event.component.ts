b22ddce641cc23b652a6476fb0c94d769cae1386
@ZedAntima <antima.gupta@zedtechnosolutions.com>
2019-05-08 11:44:59 +0530

    responsive file added

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { utilClass } from '../model/utilClass';
+import { utilClass } from '../Models/utilClass';
 import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-event',
