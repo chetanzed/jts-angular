fc4cba3325842918af745acc44839865ad2311fb
ravinderzed <ravinder.zedtechnosolutions@gmail.com>
2019-07-16 14:57:21 +0530

    add description

--- src/app/news-blog5/news-blog5.component.ts
+++ src/app/news-blog5/news-blog5.component.ts
@@ -17,10 +17,10 @@ export class NewsBlog5Component implements OnInit {
       this.firstLoad = false;
     }
     this.title.setTitle('AIがキャンセルの穴を埋めてくれる - JTS BOARD');
-    this.meta.updateTag({ name: 'discripution', content: 'みなさまこんにちは！大好きなお客様からお土産をいただきました！ブルドーザーこと、大原めぐみです。' })
+    this.meta.updateTag({ name: 'description', content: 'みなさまこんにちは！大好きなお客様からお土産をいただきました！ブルドーザーこと、大原めぐみです。'})
     this.seoService.generateTags({
       title: 'AIがキャンセルの穴を埋めてくれる - JTS BOARD',
-      discripution: 'みなさまこんにちは！大好きなお客様からお土産をいただきました！ブルドーザーこと、大原めぐみです。',
+      description: 'みなさまこんにちは！大好きなお客様からお土産をいただきました！ブルドーザーこと、大原めぐみです。',
       image: 'http://localhost:4200/assets/images/main-logo-header.png\''
     });
   }
